401153946dc3bd015124749c7016de684cb25330
YeseulChoi0 <ychoi00@stuy.edu>
2019-11-14 23:13:53 -0500

    fixed up last errors

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 #include <sys/stat.h>
+#include <time.h>
+
 
 int main(){
   struct stat stat_buffer;
   stat("makefile", &stat_buffer);
 
-  printf("Size of file: %ld bytes\n", stat_buffer.st_size);
+  printf("Size of file: %lld bytes\n", stat_buffer.st_size);
 
-  mode_t = stat_buffer.st_mode;
+  mode_t mode = stat_buffer.st_mode;
 
-  printf("Permissions: %d\n", stat_buffer.st_mode);
-  printf("Time of last access: %lld %ld\n", (long long)stat_buffer.st_atim.tv_sec, stat_buffer.st_atim.tv_nsec);
+
+  printf("Permissions: %o\n", mode %512);
+  printf("Time of last access: %s\n", ctime(&stat_buffer.st_atime));
 
 
   return 0;
